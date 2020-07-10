1dec3468515b372824241b5df3bc71440a32e0b8
lmx <liumingxin@mininglamp.com>
2020-07-10 21:27:27 +0800

    改成for 循环 但是 无法展示图片了 图片后期换成从服务器获取？

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,18 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <tarbar>
-      <tabBariItem path="/home" activeColor="blue">
-        <img slot="item-img" src="@/assets/image/home.svg"/>
-        <img slot="item-img-active" src="@/assets/image/home-active.svg"/>
-        <div slot="item-text">主页</div>
-      </tabBariItem>
-      <tabBariItem path="/category">
-        <img slot="item-img" src="@/assets/image/category.svg"/>
-        <img slot="item-img-active" src="@/assets/image/category-active.svg"/>
-        <div slot="item-text">分类</div>
-      </tabBariItem>
-      <tabBariItem path="/cart">
-        <img slot="item-img" src="@/assets/image/cart.svg"/>
-        <img slot="item-img-active" src="@/assets/image/cart-active.svg"/>
-        <div slot="item-text">购物车</div>
-      </tabBariItem>
-      <tabBariItem path="/profile">
-        <img slot="item-img" src="@/assets/image/profile.svg"/>
-        <img slot="item-img-active" src="@/assets/image/profile-active.svg"/>
-        <div slot="item-text">我的</div>
-      </tabBariItem>
-    </tarbar>
+    <main-tarbar></main-tarbar>
   </div>
 </template>
 
 <script>
-  import tarbar from "./components/tarbar/Tarbar"
-  import tabBariItem from "./components/tarbar/TarbarItem"
+
+  import mainTarbar from "./components/MainTarbar"
 
   export default {
   name: 'App',
       components:{
-          tarbar ,
-          tabBariItem
+          mainTarbar
       }
 }
 </script>
---
+++ src/components/MainTarbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <tarbar >
+    <tabBariItem v-for="(item, index) in tarbarItems" :path="item.path">
+      <img slot="item-img" :src="item.itemImg"/>
+      <img slot="item-img-active" :src="item.itemImgActive"/>
+      <div slot="item-text">{{item.name}}</div>
+    </tabBariItem>
+  </tarbar>
+</template>
+
+<script>
+    import tarbar from "./tarbar/Tarbar"
+    import tabBariItem from "./tarbar/TarbarItem"
+
+    export default {
+        name: "MainTarbar",
+        data() {
+            return {
+                tarbarItems:[
+                    {
+                        name: "主页",
+                        path: "/home",
+                        itemImg: "@/assets/image/home.svg",
+                        itemImgActive: "@/assets/image/home-active.svg"
+                    },
+                    {
+                        name: "分类",
+                        path: "/category",
+                        itemImg: "@/assets/image/category.svg",
+                        itemImgActive: "@/assets/image/category-active.svg"
+                    },
+                    {
+                        name: "购物车",
+                        path: "/cart",
+                        itemImg: "@/assets/image/cart.svg",
+                        itemImgActive: "@/assets/image/cart-active.svg"
+                    },
+                    {
+                        name: "我的",
+                        path: "/profile",
+                        itemImg: "@/assets/image/profile.svg",
+                        itemImgActive: "@/assets/image/profile-active.svg"
+                    }
+                ]
+            }
+        },
+        components: {
+            tarbar,
+            tabBariItem
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
